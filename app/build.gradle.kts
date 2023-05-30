ac2fe71ece063cd6f89fe59bdf06abee0a79ef7c
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-05-30 07:02:05 -0700

    Bump app version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soupslurpr.beautyxt"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
