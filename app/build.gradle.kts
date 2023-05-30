5705a04e385948f8515f1402b67178ce3b756fae
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-05-29 20:35:59 -0700

    Update dependencies, fix some warnings, and make version code use semantic versioning to prepare for release 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -49,13 +49,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0-alpha04")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.8.0-alpha04")
-    implementation("androidx.compose.ui:ui:1.5.0-alpha04")
-    implementation("androidx.compose.ui:ui-graphics:1.5.0-alpha04")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-alpha04")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha01")
+    implementation("androidx.compose.ui:ui:1.5.0-beta01")
+    implementation("androidx.compose.ui:ui-graphics:1.5.0-beta01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta01")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
     implementation("com.google.android.material:material:1.10.0-alpha03")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.2-alpha")
-    implementation("androidx.navigation:navigation-compose:2.6.0-rc01")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.3-beta")
+    implementation("androidx.navigation:navigation-compose:2.7.0-alpha01")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.compose.material:material:1.5.0-alpha04")
+    implementation("androidx.compose.material:material:1.5.0-beta01")
 }
--- app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/StartupScreen.kt
+++ app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/StartupScreen.kt
@@ -1,13 +1,11 @@
 package dev.soupslurpr.beautyxt.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,12 +21,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import dev.soupslurpr.beautyxt.R
 
 /**
