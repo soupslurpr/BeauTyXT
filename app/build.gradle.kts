f1f51266a4d3307d09e7fb4f9392f9183488428a
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-06-08 04:03:03 -0700

    Increase app version to 0.1.5, bump dependencies, and target sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     defaultConfig {
         applicationId = "dev.soupslurpr.beautyxt"
         minSdk = 21
-        targetSdk = 33
-        versionCode = 5
-        versionName = "0.1.4"
+        targetSdk = 34
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -55,16 +55,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0-alpha04")
+    implementation("androidx.core:core-ktx:1.12.0-alpha05")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.8.0-alpha04")
-    implementation("androidx.compose.ui:ui:1.5.0-beta01")
-    implementation("androidx.compose.ui:ui-graphics:1.5.0-beta01")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta01")
+    implementation("androidx.activity:activity-compose:1.8.0-alpha05")
+    implementation("androidx.compose.ui:ui:1.5.0-beta02")
+    implementation("androidx.compose.ui:ui-graphics:1.5.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta02")
     implementation("androidx.compose.material3:material3:1.2.0-alpha02")
     implementation("com.google.android.material:material:1.10.0-alpha03")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.3-beta")
-    implementation("androidx.navigation:navigation-compose:2.7.0-alpha01")
+    implementation("androidx.navigation:navigation-compose:2.7.0-beta01")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.compose.material:material:1.5.0-beta01")
+    implementation("androidx.compose.material:material:1.5.0-beta02")
 }
--- app/src/main/kotlin/dev/soupslurpr/beautyxt/MainActivity.kt
+++ app/src/main/kotlin/dev/soupslurpr/beautyxt/MainActivity.kt
@@ -1,7 +1,5 @@
 package dev.soupslurpr.beautyxt
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
