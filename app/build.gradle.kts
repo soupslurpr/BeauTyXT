284e1bb497a4474ca7af656ca92fda7663034472
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-06-11 05:38:28 -0700

    Increase app version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soupslurpr.beautyxt"
         minSdk = 21
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.2.0"
+        versionCode = 9
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
