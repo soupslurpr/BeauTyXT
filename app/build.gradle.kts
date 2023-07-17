1ab21c25004e787af333b49ab6afbff2cd11a4db
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-07-17 11:10:26 -0700

    Increase version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soupslurpr.beautyxt"
         minSdk = 21
         targetSdk = 34
-        versionCode = 18
-        versionName = "0.6.0"
+        versionCode = 19
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
