8f2810edd155408b78f2c511580cfc2e22a61cb5
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-07-19 11:13:23 -0700

    Update AGP to alpha 13

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-alpha12" apply false
+    id("com.android.application") version "8.2.0-alpha13" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
 }
