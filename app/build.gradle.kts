af71ceb58a95931b184bcb55dccb0c3149d20631
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-07-31 12:38:42 -0700

    Update kotlin to 1.9.0 and compose compiler to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.8"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0-alpha15" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
 }
