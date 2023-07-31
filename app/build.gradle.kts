bd073388fa70ff38cbafcce847d58a1e428a12ee
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-07-31 12:41:34 -0700

    Increase app version to 0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soupslurpr.beautyxt"
         minSdk = 21
         targetSdk = 34
-        versionCode = 26
-        versionName = "0.8.0"
+        versionCode = 27
+        versionName = "0.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
