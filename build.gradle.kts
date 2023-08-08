fc37ddeaccbe2b0bcec0786b5fc59b0bb12ac074
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-08-08 16:33:27 -0700

    Downgrade AGP from 8.2.0-alpha15 to 8.1.0-rc01

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-alpha15" apply false
+    id("com.android.application") version "8.1.0-rc01" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
 }
