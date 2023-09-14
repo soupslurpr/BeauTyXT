48af38b0015f7212213f148158045df15385ff6c
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-09-13 20:57:21 -0700

    Bump androidx.core:core-ktx from 1.10.1 to 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.navigation:navigation-compose:2.7.2")
