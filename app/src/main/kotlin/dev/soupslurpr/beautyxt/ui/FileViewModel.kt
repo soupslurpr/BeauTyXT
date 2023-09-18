9ddd4a192365633b3b61ee2ea8a40fd6b2df3dc4
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-09-18 02:26:04 -0700

    Implement printing support for plain text

--- app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/FileViewModel.kt
+++ app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/FileViewModel.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModel
 import dev.soupslurpr.beautyxt.data.FileUiState
 import dev.soupslurpr.beautyxt.markdownToDocx
 import dev.soupslurpr.beautyxt.markdownToHtml
+import dev.soupslurpr.beautyxt.sanitizeText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -238,6 +239,10 @@ class FileViewModel : ViewModel() {
 
         }
         // TODO: Handle exceptions
+    }
+
+    fun getSanitizedContent(): String {
+        return sanitizeText(uiState.value.content.value)
     }
 
     override fun onCleared() {
