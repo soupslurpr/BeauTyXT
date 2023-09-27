c229e386d62a69b3bf084b061033e702fae30fd2
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-09-27 16:26:13 -0700

    Temporarily use latest release candidate 1.8.0-rc01 for androidx.activity:activity-compose until stable 1.8.0 releases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0-rc01")
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.google.android.material:material:1.9.0")
 
