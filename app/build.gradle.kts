275f783fea00a555829b306d69102483670de8ac
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-09-27 11:10:54 -0700

    Bump androidx.navigation:navigation-compose from version 2.7.2 to 2.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.google.android.material:material:1.9.0")
 
     implementation(platform("androidx.compose:compose-bom:2023.09.01"))
