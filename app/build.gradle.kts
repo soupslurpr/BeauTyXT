599ba456df9963c73ead610347cd71d77b3dc411
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-09-27 17:51:19 -0700

    Bump androidx.compose:compose-bom from version 2023.09.01 to 2023.09.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.google.android.material:material:1.9.0")
 
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
