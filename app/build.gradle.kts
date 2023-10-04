c74ba47ceacd9c91bec86f3a46f61ae992b1b7de
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-10-03 23:08:44 -0700

    Remove duplicate datastore dependency entry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("net.java.dev.jna:jna:5.13.0@aar")
 }
