2b0deb46c81dfa651a1138f742e1a3e6457bbf9f
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-11-08 00:11:35 -0800

    Bump compose compiler from 1.5.3 to 1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
