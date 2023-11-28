611ab0b21caca034764c3a7b3ac45561d40bb21a
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-11-28 13:15:54 -0800

    Bump androidx.activity:activity-compose:1.8.0 to 1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("com.google.android.material:material:1.10.0")
     implementation("io.coil-kt:coil:2.5.0")
