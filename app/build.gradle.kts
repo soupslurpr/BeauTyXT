61c7f4ee843e8eb9abf4213d60c8fc77590dfcc6
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2023-12-07 20:37:56 -0800

    Update Compose Compiler 1.5.5 to 1.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.5"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
