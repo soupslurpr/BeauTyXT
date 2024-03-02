9da5ae3ea8be780d12f653540f023ec8af0763d8
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-03-02 10:47:55 -0800

    update Navigation Compose to 2.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.android.material:material:1.11.0")
     implementation("io.coil-kt:coil:2.5.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
