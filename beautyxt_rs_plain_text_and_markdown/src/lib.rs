1cb9018f072dda8a67a5742d711e89a17fefa3a0
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-03-17 14:28:01 -0700

    beautyxt_rs_plain_text_and_markdown: update seccomp filter

--- beautyxt_rs_plain_text_and_markdown/src/lib.rs
+++ beautyxt_rs_plain_text_and_markdown/src/lib.rs
@@ -12,34 +12,44 @@ pub fn _init() {}
 
 #[uniffi::export]
 pub fn apply_seccomp_bpf() {
-    #[cfg(target_family = "unix")]
-    let filter: BpfProgram = SeccompFilter::new(
-        vec![
-            (libc::SYS_ioctl, vec![]),
-            // Not sure why prctl is required
-            (libc::SYS_prctl, vec![]),
-            (libc::SYS_mprotect, vec![]),
-            (libc::SYS_getrandom, vec![]),
-            (libc::SYS_clock_gettime, vec![]),
-            (222, vec![]), // mmap
-        ]
-        .into_iter()
-        .collect(),
-        // mismatch_action
-        // SeccompAction::Log, // Useful for testing which syscalls are needed
-        SeccompAction::KillProcess,
-        // match_action
-        SeccompAction::Allow,
-        // target architecture of filter
-        std::env::consts::ARCH.try_into().unwrap(),
-    )
-    .unwrap()
-    .try_into()
-    .unwrap();
-
-    #[cfg(target_family = "unix")]
-    apply_filter_all_threads(&filter).unwrap();
-
     #[cfg(not(target_family = "unix"))]
-    panic!("seccomp is only available on the unix family!")
+    panic!("seccomp is only available on the unix family! Also, only unix is currently supported.");
+
+    #[cfg(target_family = "unix")]
+    {
+        let SYS_mmap: i64 = {
+            if std::env::consts::ARCH == "x86_64" {
+                9
+            } else if std::env::consts::ARCH == "aarch64" {
+                222
+            } else {
+                panic!()
+            }
+        };
+
+        let filter: BpfProgram = SeccompFilter::new(
+            vec![
+                (libc::SYS_ioctl, vec![]),
+                (libc::SYS_prctl, vec![]),
+                (libc::SYS_mprotect, vec![]),
+                (libc::SYS_getrandom, vec![]),
+                (libc::SYS_clock_gettime, vec![]),
+                (SYS_mmap, vec![]),
+            ]
+            .into_iter()
+            .collect(),
+            // mismatch_action
+            // SeccompAction::Log, // Useful for testing which syscalls are needed
+            SeccompAction::KillProcess,
+            // match_action
+            SeccompAction::Allow,
+            // target architecture of filter
+            std::env::consts::ARCH.try_into().unwrap(),
+        )
+        .unwrap()
+        .try_into()
+        .unwrap();
+
+        apply_filter_all_threads(&filter).unwrap();
+    }
 }
