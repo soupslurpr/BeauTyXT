c80338352ec0d3202d9a46a421ecfe2259a17c09
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-06-23 19:33:13 -0700

    replace deprecated AlertDialog overload usage with BasicAlertDialog

--- app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/StartupScreen.kt
+++ app/src/main/kotlin/dev/soupslurpr/beautyxt/ui/StartupScreen.kt
@@ -18,8 +18,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
@@ -225,9 +225,7 @@ fun FileTypeSelectionDialog(
     content: @Composable () -> Unit,
 ) {
     if (isShown) {
-        AlertDialog(
-            onDismissRequest = onDismissRequest,
-        ) {
+        BasicAlertDialog(onDismissRequest = onDismissRequest) {
             Surface(
                 modifier = Modifier
                     .wrapContentWidth()
