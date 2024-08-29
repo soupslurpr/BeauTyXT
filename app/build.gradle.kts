a86eb423cb105fd0240567a0936d31c5efe8f760
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-08-29 12:36:01 -0700

    update Lifecycle Runtime to 2.8.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
