1482d2fe03fb1fdffb62cf068d9e6ccb1a3361c9
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-08-29 12:02:41 -0700

    add Material Icons Extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("com.google.android.material:material:1.12.0")
